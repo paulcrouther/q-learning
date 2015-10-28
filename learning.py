0a91b16a1f2fcf216de17e737d4a92d259244242
Myung Seok Shim <ms1101.shim@gmail.com>
2015-10-27 20:24:29 -0500

    Made some functions for parameters

--- learning.py
+++ learning.py
@@ -1,24 +1,37 @@
 import random
 
 class QLearning:
-    def __init__(self, actions, epsilon, alpha, gamma):
+    def __init__(self, actions, epsilon, alpha, gamma, iterations):
         self.q = {}
         self.epsilon = epsilon
         self.alpha = alpha
         self.gamma = gamma
         self.actions = actions
+        self.iterations = iterations
+
+    def deacreasing_parameter(self):
+        if(self.alpha > 0):
+            self.alpha = self.alpha - self.alpha/self.iterations
+        else:
+            self.alpha = 0
+        if(self.epsilon > 0):        
+            self.epsilon = self.epsilon - self.epsilon/self.iterations
+        else:
+            self.epsilon = 0
 
     def getQ(self, state, action):
         return self.q.get((state, action), 0.0)
+        #return self.q.get((state, action), 0.0)
         # return self.q.get((state, action), 1.0)
 
-    def learnQ(self, state, action, reward, value):
-        oldv = self.q.get((state, action), None)
+    def learnQ(self, state, action, reward, value):    
+        oldv = self.q.get((state, action), None) 
         if oldv is None:
             self.q[(state, action)] = reward
         else:
             self.q[(state, action)] = oldv + self.alpha * (value - oldv)
-
+            print self.q[(state, action)]
+    
     def chooseAction(self, state):
         if random.random() < self.epsilon:
             action = random.choice(self.actions)
@@ -34,38 +47,8 @@ class QLearning:
 
             action = self.actions[i]
         return action
-
+    
     def learn(self, state1, action1, reward, state2):
         maxqnew = max([self.getQ(state2, a) for a in self.actions])
         self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
 
-    def printQ(self):
-        keys = self.q.keys()
-        states = list(set([a for a,b in keys]))
-        actions = list(set([b for a,b in keys]))
-        
-        dstates = ["".join([str(int(t)) for t in list(tup)]) for tup in states]
-        print (" "*4) + " ".join(["%8s" % ("("+s+")") for s in dstates])
-        for a in actions:
-            print ("%3d " % (a)) + \
-                " ".join(["%8.2f" % (self.getQ(s,a)) for s in states])
-
-    def printV(self):
-        keys = self.q.keys()
-        states = [a for a,b in keys]
-        statesX = list(set([x for x,y in states]))
-        statesY = list(set([y for x,y in states]))
-
-        print (" "*4) + " ".join(["%4d" % (s) for s in statesX])
-        for y in statesY:
-            maxQ = [max([self.getQ((x,y),a) for a in self.actions])
-                    for x in statesX]
-            print ("%3d " % (y)) + " ".join([ff(q,4) for q in maxQ])
-        
-import math
-def ff(f,n):
-    fs = "{:f}".format(f)
-    if len(fs) < n:
-        return ("{:"+n+"s}").format(fs)
-    else:
-        return fs[:n]
